0941486616a87ee6ad9ff5ab3accf8679c72d1be
PiyuminiPerera <piumininavodya08@gmail.com>
2025-03-25 12:51:56 +0530

    Added car prices

--- car.py
+++ car.py
@@ -1,9 +1,12 @@
 class Car:
     
-    def __init__(self, brand, model, year):
+    def __init__(self, brand, model, year, price):
         self.brand = brand
         self.model = model
         self.year = year
+        self.price = price
 
    def display_info(self):
-       return f"{self.year} {self.brand} {self.model}"
+       return f"{self.year} {self.brand} {self.model} - ${self.price}"
+
+   
