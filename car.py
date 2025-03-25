0e5634bf777276851e44dab89140d0a659802aa4
Nush-Faiz <nushkiyafaiz15@gmail.com>
2025-03-25 13:43:25 +0530

    Update car.py

--- car.py
+++ car.py
@@ -1,12 +1,13 @@
 class Car:
     
-    def __init__(self, brand, model, year, price):
+    def __init__(self, brand, model, year, price, speed):
         self.brand = brand
         self.model = model
         self.year = year
         self.price = price
+        self.speed = speed
 
    def display_info(self):
-       return f"{self.year} {self.brand} {self.model} - ${self.price}"
+       return f"{self.year} {self.brand} {self.model} - ${self.price}, Top Speed:{self.speed} km/h"
 
    
