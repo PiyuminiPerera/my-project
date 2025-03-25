e6da3c7ff716204a2e71233a8383baa9ceed9654
PiyuminiPerera <piumininavodya08@gmail.com>
2025-03-25 12:55:37 +0530

    Added car prices for each car

--- program.py
+++ program.py
@@ -1,9 +1,9 @@
 from car import Car
 
-car1 = Car('Toyota','Corolla',2022)
-car2 = Car('Honda','Civic',2021)
+car1 = Car('Toyota','Corolla',2022,35000)
+car2 = Car('Honda','Civic',2021,25000)
 print('Hello,Here are car details:')
 print(car1.display_info())
 print(car2.display_info())
 
-print(compare_cars(car1, car2))
+
