ae299b43b62f9fb96bceb89fc2684d1b1fa61e37
Nush-Faiz <nushkiyafaiz15@gmail.com>
2025-03-25 13:57:01 +0530

    Updated program.py to compare car speed

--- program.py
+++ program.py
@@ -1,10 +1,11 @@
 from car import Car
 
-car1 = Car('Toyota','Corolla',2022,35000)
-car2 = Car('Honda','Civic',2021,25000)
+car1 = Car('Toyota','Corolla',2022,35000,220)
+car2 = Car('Honda','Civic',2021,25000,200)
 print('Hello,Here are car details:')
 print(car1.display_info())
 print(car2.display_info())
 
 print(compare_prices(car1, car2))
 print(compare_years(car1, car2))
+print(compare_speed(car1, car2))
